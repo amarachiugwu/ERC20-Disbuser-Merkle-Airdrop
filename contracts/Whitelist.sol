5095b7dcf55e89191c50be4dde9e2e8abd759a6a
amarachiugwu <marycynthiamara2@gmail.com>
2022-10-01 15:32:07 +0100

    modified deploy.ts and claimed

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -7,14 +7,16 @@ import "./TokenA.sol";
 contract Whitelist {
 
     bytes32 public merkleRoot;
-    TokenA public token;
+    address public token;
 
-    constructor(bytes32 _merkleRoot) {
+    constructor(bytes32 _merkleRoot, address tokenAddr) {
         merkleRoot = _merkleRoot;
+        token = tokenAddr;
     }
 
     function checkInWhitelist(bytes32[] calldata proof, uint256 maxAllowanceToMint) view public returns (bool) {
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender, maxAllowanceToMint));
+        // bytes32 leaf = 0xd2fb9bc4453a931ecf93a02b841e94a7c5b2ab543f09a331e4ed7a383518d933;
         bool verified = MerkleProof.verify(proof, merkleRoot, leaf);
         return verified;
     }
@@ -25,7 +27,7 @@ contract Whitelist {
         require(checkInWhitelist(proof, maxAllowanceToMint), "You are not in the whitelist");
         require(minters[msg.sender] == false, "You have already claimed");
         minters[msg.sender] = true;
-        token.mint(msg.sender, maxAllowanceToMint);
+        TokenA(token).transfer(payable(msg.sender), maxAllowanceToMint);
     }
     
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,9 +7,7 @@ async function main() {
 
   const TokenA = await ethers.getContractFactory("TokenA");
   const tokenA = await TokenA.deploy();
-
   await tokenA.deployed();
-
   console.log(`TokenA Address ${tokenA.address}`);
 
 
@@ -28,23 +26,30 @@ async function main() {
 
 
   const Whitelist = await ethers.getContractFactory("Whitelist");
-  const whitelist = await Whitelist.deploy("0xf2e8615a03ae7e449bb4915b0b30fe6977215194fba9d832c3aee1a61e757d88");
+  const whitelist = await Whitelist.deploy("0x7acbd35877b1fb1bb1b44ce1190be0dcd824e6e2ef255f89ba1251ebecad0b9d", tokenA.address);
   await whitelist.deployed();
+  console.log(`Whitelist Address ${whitelist.address}`);
+  const interact = await ethers.getContractAt("Whitelist", whitelist.address);
+
 
   console.log("SENDING TOKEN TO AIRDROP CONTRACT");
   await tokenA.transfer(whitelist.address, ethers.utils.parseEther("2000"));
 
   const proof = proofs[whitelistAddr1].proof;
+  console.log("proof: ", proof);
 
   await helpers.setBalance(whitelistAddr1, ethers.utils.parseEther("2000000"));
-  whitelist.connect(signer).claim(proof, 20);
+  const claim = await interact.connect(signer).claim(proof, 20);
+  const result = await claim.wait();
 
+  const bal = await tokenA.balanceOf(signer.address);
 
-  console.log("CLAIMING");
+  console.log(`CLAIMING : ${result}`);
+  console.log(`CLAIMING : ${bal}`);
+
+  // const claim2 = await interact.connect(signer).claim(proof, 20); // should fail
+  // console.log(`CLAIMING : ${bal}`);  
   
-
-  // console.log(`Whitelist Address ${whitelist.address}`);
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere
